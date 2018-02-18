8df82b60b57764026b172b2b1878e5ad873e364f
Jessica Ryan <jesryan@deloitte.co.uk>
2018-02-18 19:59:27 +0000

    Adds authentication controller and passport services

--- server/models/user.js
+++ server/models/user.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
 userSchema.pre('save', function(next) {
     const user = this;
 
-    // Generate a salt
+    // Generate a salt - is like a key
     bcrypt.genSalt(10, function(err, salt) {
         if (err) { return next(err); }
         
@@ -25,6 +25,14 @@ userSchema.pre('save', function(next) {
         });
     });
 });
+
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) { return callback(err); }
+
+        callback(null, isMatch);
+    });
+}
 
 const ModelClass = mongoose.model('user', userSchema);
 
